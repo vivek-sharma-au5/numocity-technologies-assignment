0eba7c4495f8a20dbb8120b682739c2610bebc48
vivek-sharma-au5 <vivekxiix@gmail.com>
2020-10-31 10:05:44 +0530

    Problem 1 solution

---
+++ Problem 1/frontend-app/src/App.js
@@ -0,0 +1,65 @@
+import React,{useState} from 'react';
+import './App.css';
+
+const App = () => {
+  const [lunchItems,setLunchItems] = useState([])
+  const [msg ,setMsg] = useState("")
+  const [msgClass,setMsgClass] = useState("")
+
+  const handleInput = (event) => {
+    const inputData = event.target.value.replace(/^[\s,]+|[\s,]+$|\s*(\s|,)[\s,]*/g, "$1")
+    setLunchItems(inputData)
+    setMsg("")
+    setMsgClass("")
+
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault()
+      console.log(lunchItems)
+      
+      var itemsArray = lunchItems
+      if(itemsArray.length == 0){
+        setMsg("Please enter data first !!!")
+        setMsgClass("danger")
+      }
+      else if(itemsArray.length <= 3 && itemsArray.length > 0){
+        setMsg("Enjoy!!!")
+        setMsgClass("success")
+      }else if(itemsArray.length === 4){
+        setMsg("CareFull!!!")
+        setMsgClass("warning")
+      }else if(itemsArray.length >= 5){
+        setMsg("Too Much!!!")
+        setMsgClass("danger")
+      }
+     
+  }
+  return(
+    <React.Fragment >
+      <div className="App">
+      <nav className="navbar navbar-light bg-dark" >
+      <h3 style={{color:"whitesmoke"}}>Numocity Technologies</h3>
+        </nav>
+        {msg && msgClass ? ( <div className={`alert alert-${msgClass}`} role="alert">
+          {msg}
+        </div>):(<div></div>)}
+       
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="container">
+            <div className="row mt-3 col-5">
+              <input type="text" className="form-control"  placeholder="Enter Lunch items !!!" onChange={handleInput}  />
+            </div>
+            <div className="row mt-3 col-5">
+              <button type="submit" className="btn btn-success">Check if Too Much !!</button>
+            </div>
+           </div>
+        </div>
+       </form>
+      </div>
+        
+    </React.Fragment>
+  )
+}
+export default App;
